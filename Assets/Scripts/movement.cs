0a2180b054f80dbbb883d25ad83ccaca2d5343bf
Mario Pernia <57371690+mmiguelx@users.noreply.github.com>
2020-09-22 02:12:30 +0200

    jump fixed, raycasting for grounded detection

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    [SerializeField] private LayerMask ground;
+
     public Animator animator;
     public float Basespeed = 10f;
-    public float JumpForce = 400f;
+    public float JumpForce = 10f;
+
     private bool m_FacingRight = true;
     private Rigidbody2D rb;
     private Vector3 vel = Vector3.zero;
+    private CircleCollider2D cc;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        cc = GetComponent<CircleCollider2D>();
     }
+
     void Update()
     {
         if (Input.GetAxisRaw("Horizontal") == 1f)
@@ -41,22 +47,33 @@ public class movement : MonoBehaviour
             Mov(Basespeed, Input.GetAxisRaw("Horizontal"));
         }
 
-        if (Input.GetAxisRaw("Vertical") > 0)
+        //if (Input.GetAxisRaw("Vertical") > 0)
+        if (isGrounded() && Input.GetKeyDown(KeyCode.W))
         {
             animator.SetTrigger("jump");
-            rb.AddForce(new Vector2(0f, JumpForce));
+            //rb.AddForce(new Vector2(0f, JumpForce));
+            rb.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
         }
 
     }
+
     private void Mov(float b_Speed, float dir)
     {
         Vector3 targetVelocity = new Vector2(b_Speed * dir, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref vel, .05f);
     }
+
     private void Flip()
     {
         m_FacingRight = !m_FacingRight;
 
         transform.Rotate(0f, 180f, 0f);
+    }
+
+    private bool isGrounded() {
+        float extraHeight = 0.3f;
+
+        RaycastHit2D raycastHit = Physics2D.BoxCast(cc.bounds.center, cc.bounds.size, 0f, Vector2.down, extraHeight, ground);
+        return raycastHit.collider != null;
     }
 }
